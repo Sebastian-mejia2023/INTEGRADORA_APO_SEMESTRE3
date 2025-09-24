9b5279a1026ea39f1a3070776f3bd6791ee7befb
Sebastian-mejia2023 <129015950+Sebastian-mejia2023@users.noreply.github.com>
2025-09-23 22:46:15 -0500

    Cree la estructura del proyecto y un par de clases
    decidi ir adelantando la estructura del proyecto, organizando las carpetas y creando las clases principales de estas pero sin profundizar en ellas y en sus metodos

---
+++ INTEGRADORA_1/src/main/java/model/Revolver.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Revolver extends Weapon{
+    public Revolver(int damage, int shootRate) {
+        super(damage, shootRate);
+    }
+
+    @Override
+    public void shoot() {
+
+    }
+}
---
+++ INTEGRADORA_1/src/main/java/model/Shotgun.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Shotgun extends Weapon {
+
+    public Shotgun(int damage, int shootRate) {
+        super(damage, shootRate);
+    }
+
+    @Override
+    public void shoot() {
+
+    }
+
+
+
+}
